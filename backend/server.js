a2d0366eee129514a569f1660851c480fcce6676
ZacharyFolk <zacharyfolk@gmail.com>
2020-12-26 13:04:49 -0800

    Established db connection with Atlas

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }); // options because of updates to MongoDB
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log("Woo hoo! MongoDB database connection established.")
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
